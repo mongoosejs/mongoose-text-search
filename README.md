6a537507ae3328144edb63689f752ab76a4d8d3a
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2013-03-25 13:20:30 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,95 @@
+#mongoose-text-search
+======================
+
+Provides MongoDB 2.4 text search support for mongoose.
+
+[![Build Status](https://travis-ci.org/aheckmann/mongoose-function.png?branch=master)](http://travis-ci.org/aheckmann/mongoose-text-search)
+
+## Example:
+
+```js
+// modules
+var mongoose = require('mongoose');
+var textSearch = require('mongoose-text-search');
+
+// create our schema
+var gameSchema = mongoose.Schema({
+    name: String
+  , tags: [String]
+  , likes: Number
+  , created: Date
+});
+
+// give our schema text search capabilities
+gameSchema.plugin(textSearch);
+
+// add a text index to the tags array
+gameSchema.index({ tags: 'text' });
+
+// test it out
+var Game = mongoose.model('Game', gameSchema);
+
+Game.create({ name: 'Super Mario 64', tags: ['nintendo', 'mario', '3d'] }, function (err) {
+  if (err) return handleError(err);
+
+  Game.textSearch('3d', function (err, output) {
+    if (err) return handleError(err);
+
+    var inspect = require('util').inspect;
+    console.log(inspect(output, { depth: null }));
+
+    // { queryDebugString: '3d||||||',
+    //   language: 'english',
+    //   results:
+    //    [ { score: 1,
+    //        obj:
+    //         { name: 'Super Mario 64',
+    //           _id: 5150993001900a0000000001,
+    //           __v: 0,
+    //           tags: [ 'nintendo', 'mario', '3d' ] } } ],
+    //   stats:
+    //    { nscanned: 1,
+    //      nscannedObjects: 0,
+    //      n: 1,
+    //      nfound: 1,
+    //      timeMicros: 77 },
+    //   ok: 1 }
+  });
+});
+```
+
+### Output:
+
+The output is not limited to the found documents themselves but also the complete details of the executed command.
+
+The `results` property of the output is an array of objects containing the found document and its corresponding search ranking. `score` is the ranking, `obj` is the [mongoose document](http://mongoosejs.com/docs/documents.html).
+
+For more information about these properties, read the [MongoDB documentation](http://docs.mongodb.org/manual/reference/text-search/#text-search-output).
+
+## Options
+
+`mongoose-text-search` supports passing an options object as the second argument.
+
+- `project`: select which [fields](http://docs.mongodb.org/manual/reference/command/text/) to return (mongoose [field selection](http://mongoosejs.com/docs/api.html#query_Query-select) syntax supported)
+- `filter`: declare an additional [query matcher](http://docs.mongodb.org/manual/reference/command/text/) using `find` syntax (arguments are cast according to the schema).
+- `limit`: [maximum number](http://docs.mongodb.org/manual/reference/command/text/) of documents (mongodb default is 100)
+- `language`: change the [search language](http://docs.mongodb.org/manual/reference/command/text/)
+
+Example:
+
+```js
+var options = {
+    project: '-created' // do not include the `created` property
+  , filter: { likes: { $gt: 1000000 }}
+  , limit: 10
+  , language: 'spanish'
+}
+
+Game.textSearch('game -mario', options, callback);
+```
+
+## Notes:
+
+As of mongoose 3.6.0, text indexes must be added using the [Schema.index()](http://mongoosejs.com/docs/api.html#schema_Schema-index) method.
+
+As of MongoDB 2.4.0, [text search](http://docs.mongodb.org/manual/applications/text-search/) is experimental/beta. As such, this functionality is not in mongoose core.
---
+++ test/index.js
@@ -0,0 +1,172 @@
+var assert = require('assert')
+var mongoose = require('mongoose')
+var uri = 'mongodb://localhost/test-mongoose-text-search';
+var textSearch = require('../')
+
+function getSchema () {
+  var s = mongoose.Schema({
+      single: String
+    , array: [String]
+  })
+  s.index({ single: 'text', array: 'text' })
+  return s;
+}
+
+function makeDocs () {
+  var ret = [];
+  ret.push({
+      single: 'Blueberry'
+    , array: ['array', 'of', 'strings']
+  })
+  ret.push({
+      single: 'elephant a string'
+    , array: ['1']
+  })
+  ret.push({
+      single: 'a significant word ice cream funny'
+  })
+  return ret;
+}
+
+describe('mongoose-text-search', function(){
+  it('is a function', function(done){
+    assert.equal('function', typeof textSearch);
+    done();
+  })
+
+  it('adds a textSearch method to the schema', function(done){
+    var s = getSchema();
+    s.plugin(textSearch);
+    assert.equal('function', typeof s.statics.textSearch);
+    done();
+  })
+
+  it('has a version', function(done){
+    assert.equal('string', typeof textSearch.version);
+    done();
+  })
+})
+
+describe('mongoose-text-search integration', function(){
+  var db;
+  var schema;
+  var modelName = 'Test';
+  var model, blueberry, elephant, letters;
+
+  before(function(done){
+    var m = new mongoose.Mongoose;
+    db = m.createConnection(uri);
+    db.once('error', done);
+    db.once('open', function () {
+      schema = getSchema();
+      schema.plugin(textSearch);
+      model = db.model(modelName, schema);
+      model.on('index', function (err) {
+        assert.ifError(err);
+        model.create(makeDocs(), function (err, blue, eleph, zzz) {
+          if (err) return done(err);
+          blueberry = blue;
+          elephant = eleph;
+          letters = zzz;
+          done();
+        });
+      })
+    })
+  })
+  after(function(done){
+    db.db.dropDatabase(function(){
+      db.close(done)
+    })
+  })
+
+  it('requires a callback', function(done){
+    assert.throws(function(){
+      model.textSearch('stuff');
+    })
+    assert.throws(function(){
+      model.textSearch('stuff', { });
+    })
+    done();
+  })
+  it('requires a search', function(done){
+    model.textSearch({ }, function (err) {
+      assert.ok(err);
+      done();
+    })
+  })
+
+  it('casts results to mongoose documents', function(done){
+    model.textSearch('blueberry', function (err, res) {
+      assert.ifError(err);
+      assert.ok(res);
+      assert.ok(Array.isArray(res.results));
+      res.results.forEach(function (result) {
+        assert.ok(result.obj instanceof mongoose.Document);
+      })
+      assert.equal(1, res.results.length);
+      assert.equal(blueberry.id, res.results[0].obj.id);
+      done();
+    })
+  })
+
+  it('accepts limit', function(done){
+    model.textSearch('strings', { limit: 1 }, function (err, res) {
+      assert.ifError(err);
+      assert.ok(res);
+      assert.ok(Array.isArray(res.results));
+      assert.equal(1, res.results.length);
+      assert.equal(blueberry.id, res.results[0].obj.id);
+      done();
+    })
+  })
+
+  it('accepts filter (and casts)', function(done){
+    model.textSearch('strings', { filter: { array: [1] } }, function (err, res) {
+      assert.ifError(err);
+      assert.ok(res);
+      assert.ok(Array.isArray(res.results));
+      assert.equal(1, res.results.length);
+      assert.equal(elephant.id, res.results[0].obj.id);
+      done();
+    })
+  })
+
+  describe('accepts project', function(){
+    it('with object syntax', function(done){
+      model.textSearch('funny', { project: {single: 0}}, function (err, res) {
+        assert.ifError(err);
+        assert.ok(res);
+        assert.ok(Array.isArray(res.results));
+        assert.equal(1, res.results.length);
+        assert.equal(letters.id, res.results[0].obj.id);
+        assert.equal(undefined, res.results[0].obj.single);
+        done();
+      })
+    })
+    it('with string syntax', function(done){
+      model.textSearch('funny', { project: '-single'}, function (err, res) {
+        assert.ifError(err);
+        assert.ok(res);
+        assert.ok(Array.isArray(res.results));
+        assert.equal(1, res.results.length);
+        assert.equal(letters.id, res.results[0].obj.id);
+        assert.equal(undefined, res.results[0].obj.single);
+        done();
+      })
+    })
+  })
+
+  it('accepts language', function(done){
+    model.textSearch('funny', { language: 'spanish'}, function (err, res) {
+      assert.ifError(err);
+      assert.ok(res);
+      assert.ok(Array.isArray(res.results));
+      assert.equal(0, res.results.length);
+      done();
+    })
+  })
+
+})
+
+// if mongoose >= 3.6.1 text index type is supported
+
